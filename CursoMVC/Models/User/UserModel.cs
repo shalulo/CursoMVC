6798b512a2cf1c2a14b4642b18f7a818b32e3e52
Shalulo <saulsuarezr@gmail.com>
2021-05-26 15:43:42 -0500

    Se adiciona funcionalidad de edit y delete en las listas

--- CursoMVC/Models/User/UserModel.cs
+++ CursoMVC/Models/User/UserModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +10,24 @@ namespace CursoMVC.Models.User
     public class UserModel
     {
         public string Nombre { get; set; }
+
         public string Apellido { get; set; }
+
+        [Key]
         public string Cedula { get; set; }
+
         public string Email { get; set; }
+
         public string Perfil { get; set; }
+
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+    }
+
+    public class UserDBContext : DbContext
+    {
+        public UserDBContext() { }
+
+        public DbSet<UserModel> Users{ get; set; }
     }
 }
